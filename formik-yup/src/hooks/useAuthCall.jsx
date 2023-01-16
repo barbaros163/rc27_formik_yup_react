812234d98fb2e7cf5885e14806991582b69bd48b
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-17 00:26:14 +0100

    logout

--- formik-yup/src/hooks/useAuthCall.jsx
+++ formik-yup/src/hooks/useAuthCall.jsx
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+} from "../features/authSlice";
 import { useDispatch } from "react-redux";
 
 const BASE_URL = "https://clarusway.pythonanywhere.com/";
@@ -8,20 +13,31 @@ const useAuthCall = () => {
   const dispatch = useDispatch();
 
   const login = async (userInfo) => {
+    dispatch(fetchStart());
     try {
-      dispatchEvent(fetchStart());
       const { data } = await axios.post(
-        `${BASE_URL}account/auth/login`,
+        `${BASE_URL}account/auth/login/`,
         userInfo
       );
-      dispatchEvent(loginSuccess(data));
+      dispatch(loginSuccess(data));
     } catch (error) {
       console.log(error);
-      dispatchEvent(fetchFail());
+      dispatch(fetchFail());
     }
   };
 
-  return { login };
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      await axios.post(`${BASE_URL}account/auth/logout/`);
+      dispatch(logoutSuccess());
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  return { login, logout };
 };
 
 export default useAuthCall;
